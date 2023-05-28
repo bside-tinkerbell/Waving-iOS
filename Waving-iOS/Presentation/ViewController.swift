dc056be36033ce6121e39138b845a0f7f6aeffc1
JOY <jypjoy@gmail.com>
2023-05-28 20:16:24 +0900

    ✨ feat: Snapkit라이브러리 사용을 위한 interface 추가

--- Waving-iOS/Presentation/ViewController.swift
+++ Waving-iOS/Presentation/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SnapKitInterface{
+    
+    private let loginButton = UIButton().then {
+        $0.setTitle("로그인 버튼", for: .normal)
+        $0.setTitleColor(.systemBlue, for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBackground
+        addComponents()
+        setConstraints()
+    }
+    
+    func addComponents() {
+        view.addSubview(loginButton)
+    }
+    
+    func setConstraints() {
+        loginButton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
     }
 
 }
