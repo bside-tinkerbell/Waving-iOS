3706e77584b0b59059d3cc20a768a97a378a0b6e
JOY <jypjoy@gmail.com>
2023-05-28 19:39:08 +0900

    chore: 필요한 라이브러리 추가
    Moya, RxSwift, Snapkit, Then

--- Waving-iOS/Presentation/ViewController.swift
+++ Waving-iOS/Presentation/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemGreen
     }
 
 }
