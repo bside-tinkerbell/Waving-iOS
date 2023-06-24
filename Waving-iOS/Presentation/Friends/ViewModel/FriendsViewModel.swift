34bf21850f03328cbed9a9961c487b8d280f24f3
JOY <jypjoy@gmail.com>
2023-06-25 04:15:03 +0900

    feat: contact 프레임워크 연결 추가

--- Waving-iOS/Presentation/Friends/ViewModel/FriendsViewModel.swift
+++ Waving-iOS/Presentation/Friends/ViewModel/FriendsViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Combine
 import UIKit
+import Contacts
 
 enum FriendType: Int {
     case intro
@@ -43,6 +44,29 @@ class FriendsViewModel: FriendsViewModelRepresentable {
     
     func addFriends() {
         Log.d("친구 추가")
+        Task.init {
+            await fetchAllContacts()
+        }
+        
+        @Sendable func fetchAllContacts() async {
+            let store = CNContactStore()
+            let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactImageDataKey] as [CNKeyDescriptor]
+      
+            let fetchRequest = CNContactFetchRequest(keysToFetch: keys)
+
+            do {
+                try store.enumerateContacts(with: fetchRequest, usingBlock: { contact, result in
+                    let name = contact.familyName + contact.givenName
+                    let phoneNumber = contact.phoneNumbers.filter { $0.label == CNLabelPhoneNumberMobile}.map {$0.value.stringValue}.joined(separator:"")
+                    Log.i(name)
+                    Log.d(phoneNumber)
+                    //TODO: 프로필 이미지 정보 API 보낼 수 있게 String화 하기 (contact.imageData) - decoding?
+                })
+            } catch {
+                Log.e("연락처를 가져올 수 없습니다 화면으로 이동하기")
+            }
+        }
+        
     }
     
     func didTapBackButton() {
