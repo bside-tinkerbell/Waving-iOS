a35fac27e11baddf7414801f65f1fa7e51d48997
JOY <jypjoy@gmail.com>
2023-08-07 03:51:28 +0900

    feat: 지인 프로필  관련 파일 폴더링

---
+++ Waving-iOS/Presentation/FriendProfile/View/BottomProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomProfileView.swift
+//  Waving-iOS
+//
+//  Created by Joy on 2023/08/07.
+//
+
+import UIKit
+
+class BottomProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
