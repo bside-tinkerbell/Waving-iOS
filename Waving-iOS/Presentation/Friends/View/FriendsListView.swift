b8aeac6e4a6bb90e1fdd58b6e1378eebd7dc7501
JOY <jypjoy@gmail.com>
2023-08-07 14:28:17 +0900

    feat: FriendListView 3부분으로 divide

--- Waving-iOS/Presentation/Friends/View/FriendsListView.swift
+++ Waving-iOS/Presentation/Friends/View/FriendsListView.swift
@@ -20,19 +20,11 @@ class FriendsListView: UIView, SnapKitInterface {
         return view
     }()
     
-    private lazy var friendscontactCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.estimatedItemSize = .zero
-        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        view.register(FriendsContactCollectionViewCell.self, forCellWithReuseIdentifier: FriendsContactCollectionViewCell.identifier)
-
-        view.dataSource = self
-        view.delegate = self
-        return view
-    }()
+    private var suggestionView: UIView? 
+    private var favoriteView: UIView?
+    private var contactListView: UIView? //UICollectionView 위치
     
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addComponents()
@@ -47,7 +39,7 @@ class FriendsListView: UIView, SnapKitInterface {
         backgroundColor = .systemBackground
         addSubview(scrollView)
         scrollView.addSubview(containerView)
-        containerView.addSubview(friendscontactCollectionView)
+//        containerView.addSubview(friendscontactCollectionView)
     }
     
     func setConstraints() {
@@ -60,16 +52,7 @@ class FriendsListView: UIView, SnapKitInterface {
             $0.width.equalTo(scrollView)
             $0.height.equalTo(scrollView).priority(.low)
         }
-        
-        friendscontactCollectionView.snp.makeConstraints {
-            $0.top.equalTo(containerView)
-            $0.leading.equalTo(containerView).offset(20)
-            $0.trailing.equalTo(containerView).offset(-20)
-            $0.bottom.equalTo(containerView)
-        }
-        
     }
-
 }
 
 extension FriendsListView: FriendViewRepresentable {
@@ -79,32 +62,3 @@ extension FriendsListView: FriendViewRepresentable {
 }
 
 
-extension FriendsListView: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FriendsContactCollectionViewCell.identifier, for: indexPath) as? FriendsContactCollectionViewCell else { fatalError() }
-        cell.configUI(.none)
-        return cell
-    }
-}
-
-extension FriendsListView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.viewModel?.didTapProfile()
-    }
-}
-
-
-extension FriendsListView: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellWidth = collectionView.frame.size.width
-        return CGSize(width: cellWidth, height: cellWidth/5.9)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        0
-    }
-}
