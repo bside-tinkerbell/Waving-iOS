40ad8b2167f249123f6438eea3db64426f03d740
JOY <jypjoy@gmail.com>
2023-08-07 10:49:58 +0900

    feat: 지인 프로필 base UI 구성

--- Waving-iOS/Presentation/FriendProfile/ViewController/FriendProfileViewController.swift
+++ Waving-iOS/Presentation/FriendProfile/ViewController/FriendProfileViewController.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import Combine
 
 class FriendProfileViewController: UIViewController {
+    
+    private lazy var navigationViewModel: NavigationModel = .init(backButtonImage: UIImage(named: "icn_back"), favoriteButtonImage: UIImage(named:"icn_favorites_off"), forwaredButtonImage: UIImage(named:"icn_edit"), title: "프로필") {[weak self] in
+        Log.d("뒤로 가기")
+    } didTouchFavorite: { [weak self] in
+        Log.d("즐겨찾기")
+    } didTouchForwared: { [weak self] in
+        Log.d("편집")
+    }
 
+    private lazy var navigationView: NavigationView = {
+        let view = NavigationView()
+        view.setup(model: navigationViewModel)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let scrollView: UIScrollView = {
+       let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var containerView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .gray020
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addComponents()
+        setConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addComponents() {
+        view.backgroundColor = .systemBackground
+        self.navigationController?.navigationBar.isHidden = true
+        [navigationView, scrollView].forEach { view.addSubview($0) }
+        scrollView.addSubview(containerView)
     }
-    */
-
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            navigationView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationView.heightAnchor.constraint(equalToConstant: 50),
+            
+            scrollView.topAnchor.constraint(equalTo: navigationView.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            containerView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
+        let containerViewHeight = containerView.heightAnchor.constraint(greaterThanOrEqualTo: scrollView.heightAnchor)
+        containerViewHeight.priority = .defaultLow
+        containerViewHeight.isActive = true
+    }
 }
