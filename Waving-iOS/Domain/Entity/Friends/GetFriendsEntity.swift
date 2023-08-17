5a1fc2680d87e54110f21a572c4f0f878526e561
JOY <jypjoy@gmail.com>
2023-08-17 12:54:33 +0900

    feat: GetFriendsEntity 추가

---
+++ Waving-iOS/Domain/Entity/Friends/GetFriendsEntity.swift
@@ -0,0 +1,13 @@
+//
+//  GetFriendsEntity.swift
+//  Waving-iOS
+//
+//  Created by Joy on 2023/08/17.
+//
+
+import Foundation
+
+struct GetFriendsEntity {
+    let name: String
+    let friendProfileId: String
+}
