b47fd102e3119216f730f2606a09dadf8589d02f
JOY <jypjoy@gmail.com>
2023-09-26 10:36:21 +0900

    feat: user_id 로그인 후 불러올 수 있는 데이터로 바꿈

--- Waving-iOS/Data/API/Friends/FriendsEndpoint.swift
+++ Waving-iOS/Data/API/Friends/FriendsEndpoint.swift
@@ -20,7 +20,8 @@ enum FriendsEndpoint: APIEndpoint {
         case .saveFriends:
             return "/v1/friends/register"
         case .getFriends:
-            return "/v1/friends/list/16" //TODO: 임의적(맨 뒤 user_id 들어가야 함)
+            //return "/v1/friends/list/16" //TODO: 임의적(맨 뒤 user_id 들어가야 함)
+            return "/v1/friends/list/" + "\(LoginDataStore.shared.userId!)"
         }
     }
     
@@ -43,8 +44,10 @@ enum FriendsEndpoint: APIEndpoint {
     var parameters: [String: Any]? {
         switch self {
         case .saveFriends:
-            let saveFriendsDTO = SaveFriendsDTO(userId: 16, contactId: 22, profileList: [PersonModel(name: "가나다", cellPhone: "010-1234-5678", contactCycle: 2)]) //TODO: API 작동 실험용
-
+           // let saveFriendsDTO = SaveFriendsDTO(userId: 16, contactId: 22, profileList: [PersonModel(name: "가나다", cellPhone: "010-1234-5678", contactCycle: 2)]) //TODO: API 작동 실험용
+            
+            let saveFriendsDTO = SaveFriendsDTO(userId: LoginDataStore.shared.userId!, contactId: 22, profileList: [PersonModel(name: "가나다", cellPhone: "010-1234-5678", contactCycle: 2)]) //TODO: 뒤에 들어가는 사람 목록 바꿔야 함
+            
             return saveFriendsDTO.asDictionary
         case .getFriends:
             return nil
