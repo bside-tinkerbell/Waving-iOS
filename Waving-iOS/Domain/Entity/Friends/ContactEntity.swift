99fe5f18d4c30ccbf90026fc4bdc377bb1e3f3c4
JOY <jypjoy@gmail.com>
2023-10-01 06:43:54 +0900

    feat: 지인리스트 목록에서 contactId 따로 추출

--- Waving-iOS/Domain/Entity/Friends/ContactEntity.swift
+++ Waving-iOS/Domain/Entity/Friends/ContactEntity.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 var myContactList = [ContactEntity]()
+var saveContactList = [ContactEntity]()
 
 struct ContactEntity {
     let name: String
     let phoneNumber: String
+    let contactCycle: Int = 2
+}
+
+struct SaveContactEntity {
+    static var shared = SaveContactEntity()
+    
+    var contactId: Int = 0
 }
