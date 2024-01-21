2d8345b95476df1e64051f0356013db2513b4274
JOY <jypjoy@gmail.com>
2024-01-21 22:01:34 +0900

    feat: 가입 > 비밀번호 제약 조건 수정

--- Waving-iOS/Util/Extension/String+.swift
+++ Waving-iOS/Util/Extension/String+.swift
@@ -12,7 +12,8 @@ extension String {
         NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
     }
     
+    // 영문/숫자/특수문자 2가지 이상 조합하세요.(8~20자)
     var isValidPassword: Bool {
-        NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,20}$").evaluate(with: self)
+        NSPredicate(format: "SELF MATCHES %@", "^((?:(?=.*[A-Za-z])(?=.*[$@$!%*?&])|(?:(?=.*[A-Za-z])(?=.*\\d)))|(?:(?=.*[$@$!%*?&])(?=.*\\d))).{8,20}$").evaluate(with: self)
     }
 }
